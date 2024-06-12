e316136d43bf444e106aca63aa2a626825ee7344
suzangyawali <sujangyawali17@gmail.com>
2024-06-12 16:52:00 +0545

    Initial commit of the Tic-Tac-Toe game project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Grid from './components/Grid/grid';
+// import Card from './components/card/card'
+
+function App() {
+  
+  return (
+    <>
+    <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App;
